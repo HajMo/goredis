591cc78178b309dea648f11a76ddd64d24d6bbc9
HajMo <50548630+HajMo@users.noreply.github.com>
2023-03-27 12:30:25 +0200

    Respond to PING

--- app/server.go
+++ app/server.go
@@ -8,16 +8,27 @@ import (
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	
+
+	defer l.Close()
+
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer connection.Close()
+
+	buffer := make([]byte, 1024)
+
+	if _, err := connection.Read(buffer); err != nil {
+		fmt.Println("error reading from client: ", err.Error())
+		os.Exit(1)
+	}
+
+	connection.Write([]byte("+PONG\r\n"))
 }
