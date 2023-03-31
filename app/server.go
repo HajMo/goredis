74d07a496e72aa79da63e9688a63ec24b1357910
HajMo <50548630+HajMo@users.noreply.github.com>
2023-03-31 23:14:48 +0200

    Fixes

--- app/server.go
+++ app/server.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,6 +18,13 @@ func main() {
 	}
 
 	defer listener.Close()
+
+	f, err := os.Create("storage")
+	if err != nil {
+		fmt.Println("Failed to create storage file")
+		os.Exit(1)
+	}
+	f.Close()
 
 	for {
 		connection, err := listener.Accept()
@@ -49,8 +57,64 @@ func handleConnection(connection net.Conn) {
 			connection.Write([]byte("+PONG\r\n"))
 		case "echo":
 			connection.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[0].String()), args[0].String())))
+		case "set":
+			if len(args) != 2 {
+				fmt.Println("Error handling set command")
+				connection.Write([]byte("-ERR handling command: '" + command + "'\r\n"))
+				return
+			}
+			if _, err := handleSetCommand(args[0], args[1]); err != nil {
+				fmt.Println("Error handling set command: ", err.Error())
+				connection.Write([]byte("-ERR handling command: '" + command + "'\r\n"))
+				os.Exit(1)
+			}
+			connection.Write([]byte("+OK\r\n"))
+		case "get":
+			value, _ := handleGetCommand(args[0].String())
+			connection.Write([]byte("+" + value + "\r\n"))
 		default:
 			connection.Write([]byte("-ERR unknown command '" + command + "'\r\n"))
 		}
 	}
+}
+
+func handleSetCommand(key Value, value Value) (int, error) {
+	file, err := os.OpenFile("storage", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	n, err := file.WriteString(fmt.Sprintf("%s:%s\n", key.String(), value.String()))
+	if err != nil {
+		panic(err)
+	}
+
+	return n, err
+}
+
+func handleGetCommand(key string) (string, error) {
+	file, err := os.OpenFile("storage", os.O_RDONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	values := []string{}
+
+	for scanner.Scan() {
+		scannedKey := strings.Split(scanner.Text(), ":")
+		if strings.Contains(scannedKey[0], key) && len(scannedKey[0]) == len(key) {
+			values = append(values, scanner.Text())
+		}
+	}
+
+	if len(values) == 0 {
+		return "", nil
+	}
+
+	return strings.Split(values[len(values)-1], ":")[1], nil
 }
